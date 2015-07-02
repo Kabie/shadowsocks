f7e7e0f0ace5bf08c1df19a7c793641b2256f79f
Kabie <kabie3000@gmail.com>
2015-07-02 21:18:52 +0800

    fix syntax

--- lib/shadowsocks/coder.ex
+++ lib/shadowsocks/coder.ex
@@ -6,7 +6,7 @@ defmodule ShadowSocks.Coder do
   def evp_bytes_to_key(password, key_len, iv_len) do
     _evp_bytes_to_key(password, key_len, iv_len, "")
   end
-  
+
   defp _evp_bytes_to_key(password, key_len, iv_len, bytes) do
     case bytes do
       <<key::binary-size(key_len), iv::binary-size(iv_len), _::binary>> ->
@@ -17,7 +17,7 @@ defmodule ShadowSocks.Coder do
     end
   end
 
-  def new(key, iv) do: :crypto.stream_init :aes_ctr, key, iv
+  def new(key, iv), do: :crypto.stream_init(:aes_ctr, key, iv)
   defdelegate encode(encoder, bytes), to: :crypto, as: :stream_encrypt
   defdelegate decode(decoder, bytes), to: :crypto, as: :stream_decrypt
 
