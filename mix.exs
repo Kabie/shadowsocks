5c611a193101ef745c8dc20d392015ee8af8397d
Kabie <kabie3000@gmail.com>
2016-09-07 10:11:30 +0800

    use distillery

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShadowSocks.Mixfile do
   def project do
     [app: :shadowsocks,
      version: "0.3.2",
-     elixir: "~> 1.1",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -29,7 +29,7 @@ defmodule ShadowSocks.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:exrm, "~> 0.19.9", only: :prod}
+      {:distillery, "~> 0.9"}
     ]
   end
 end
